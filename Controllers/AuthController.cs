882b2edd2c8f968e7a6ca1a99ca60dfad4c80c43
Vinicius <vini383@gmail.com>
2023-12-20 14:30:39 -0300

    feat: added mock user to create bearer token

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using JwtAspNet.Services;
+﻿using JwtAspNet.Models;
+using JwtAspNet.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace JwtAspNet.Controllers
 {
@@ -10,8 +12,17 @@ namespace JwtAspNet.Controllers
         [HttpGet("login")]
         public string Login(AuthService service)
         {
-            var token = service.CreateToken();
+            var user = new User(Id: 1,
+                Email: "Vini123@gmail.com",
+                Name: "Vinícius",
+                Password: Guid.NewGuid().ToString(),
+                Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH9Hy0DRPnkdxOSN89rOI7IdevMB76D3pUhDHcz9d7KA&s",
+                Roles: new[]
+                {
+                    "Admin", "User", "Author", "Student"
+                });
 
+            var token = service.CreateToken(user);
             return token;
         }
     }
