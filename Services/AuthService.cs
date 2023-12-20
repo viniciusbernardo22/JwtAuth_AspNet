532a01794498a1fb2cf66151444eb500d97ca12d
Vinicius <vini383@gmail.com>
2023-12-20 14:31:25 -0300

    added claims generator services by userinfo

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using JwtAspNet.Models;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace JwtAspNet.Services
@@ -7,7 +9,7 @@ namespace JwtAspNet.Services
     public class AuthService
     {
         //Method that will generate the Token
-        public string CreateToken()
+        public string CreateToken(User user)
         {
             var handler = new JwtSecurityTokenHandler();
 
@@ -22,12 +24,29 @@ namespace JwtAspNet.Services
             {
                 SigningCredentials = credentials,
                 Expires = DateTime.UtcNow.AddHours(2),
+                Subject = GenerateClaims(user)
 
             };
 
             var token = handler.CreateToken(tokenDescriptor);
 
             return handler.WriteToken(token);
+        }
+   
+        private static ClaimsIdentity GenerateClaims(User user)
+        {
+            ClaimsIdentity CI = new ();
+
+            CI.AddClaim(new Claim("Id", user.Id.ToString()));
+            CI.AddClaim(new Claim(ClaimTypes.Name, user.Email));
+            CI.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+            CI.AddClaim(new Claim(ClaimTypes.GivenName, user.Name));
+            CI.AddClaim(new Claim("Image", user.Image));
+
+            foreach (var role in user.Roles)
+                CI.AddClaim(new Claim(ClaimTypes.Role, role));
+            
+            return CI;
         }
     }
 }
